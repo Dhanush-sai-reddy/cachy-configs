5af80f41d2def03736e106df95fb02964d999a91
Dhanush-sai-reddy <dhanushsaireddy777@gmail.com>
2026-06-01 16:35:31 +0530

    local configs

--- install-scripts/dotfiles-main.sh
+++ install-scripts/dotfiles-main.sh
@@ -19,19 +19,13 @@ fi
 # Check if Hyprland-Dots exists
 printf "${NOTE} Cloning and Installing ${SKY_BLUE}KooL's Hyprland Dots${RESET}....\n"
 
-if [ -d Hyprland-Dots ]; then
-  cd Hyprland-Dots
-  git stash && git pull
+rm -rf Hyprland-Dots
+if cp -r local-hypr-configs ./Hyprland-Dots; then
+  cd Hyprland-Dots || exit 1
   chmod +x copy.sh
   ./copy.sh 
 else
-  if cp -r ../local-hypr-configs ./Hyprland-Dots; then
-    cd Hyprland-Dots || exit 1
-    chmod +x copy.sh
-    ./copy.sh 
-  else
-    echo -e "$ERROR Can't download ${YELLOW}KooL's Hyprland-Dots${RESET} . Check your internet connection"
-  fi
+  echo -e "$ERROR Can't copy local-hypr-configs"
 fi
 
 printf "\n%.0s" {1..2}
